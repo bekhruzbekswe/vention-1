b6dfbff7803e0997af76b2dd793cd66ebeca963d
bekhruzbekswe <bekhruzbekswe@gmail.com>
2025-05-13 11:11:06 +0500

    show covered with bem--responsive

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -3,24 +3,23 @@
 @section('title', 'View Contact')
 
 @section('content')
-<div class="container">
-    <h1>Contact Details</h1>
+<div class="contact-view">
+    <h1 class="contact-view__title contact-view__name">{{ $contact->name }}</h1>
 
-    <div >
-        <div class="card-body">
-            <h5 class="form-input">{{ $contact->name }}</h5>
-            <p class="form-input"><strong>Email:</strong> {{ $contact->email }}</p>
-            <p class="form-input"><strong>Phone:</strong> {{ $contact->phone }}</p>
-            <p class="form-input"><strong>Notes:</strong> {{ $contact->notes }}</p>
-        </div>
+    <div class="contact-view__details">
+        <p class="contact-view__item"><strong>Email:</strong> {{ $contact->email }}</p>
+        <p class="contact-view__item"><strong>Phone:</strong> {{ $contact->phone }}</p>
+        <p class="contact-view__item"><strong>Notes:</strong> {{ $contact->notes }}</p>
     </div>
-    <div class="buttons">
-        <a href="{{ route('contacts.index') }}" class="cancel-button action-button">Home</a>
-        <a href="{{ route('contacts.edit', $contact) }}" class="edit-button mobile-only action-button" id="show-edit">Edit</a>
-        <form action="{{ route('contacts.destroy', $contact) }}" method="POST" id="show-delete" class="delete-form mobile-only">
+
+    <div class="contact-view__actions">
+        <a href="{{ route('contacts.index') }}" class="contact-view__button contact-view__button--home contacts-table__action-button">Home</a>
+        <a href="{{ route('contacts.edit', $contact) }}" class="contact-view__button contact-view__button--edit contact-view__button--mobile contacts-table__action-button">Edit</a>
+        
+        <form action="{{ route('contacts.destroy', $contact) }}" method="POST" class="contact-view__form contact-view__form--delete contact-view__button--mobile ">
             @csrf
             @method('DELETE')
-            <button onclick="return confirm('Delete this contact?')" class="delete-button action-button">Delete</button>
+            <button onclick="return confirm('Delete this contact?')" class="contact-view__button contact-view__button--delete contacts-table__action-button">Delete</button>
         </form>
     </div>
 </div>
