e9fa48be1d3c81971dcc58732ad4815dad21ffc0
bekhruzbekswe <bekhruzbekswe@gmail.com>
2025-05-13 10:25:08 +0500

    Index page is now responsive

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -9,20 +9,20 @@
             <thead class="contacts-table__head">
                 <tr class="contacts-table__header-row">
                     <th class="contacts-table__header">Name</th>
-                    <th class="contacts-table__header">Email</th>
+                    <th class="contacts-table__header contacts__header--email">Email</th>
                     <th class="contacts-table__header">Phone</th>
-                    <th class="contacts-table__header">Actions</th>
+                    <th class="contacts-table__header contacts__header--actions">Actions</th>
                 </tr>
             </thead>
             <tbody class="contacts-table__body">
                 @foreach($contacts as $contact)
                     <tr class="contacts-table__row contacts-table__row--clickable" onclick="window.location='{{ route('contacts.show', $contact) }}'">
                         <td class="contacts-table__cell">{{ $contact->name }}</td>
-                        <td class="contacts-table__cell">{{ $contact->email }}</td>
+                        <td class="contacts-table__cell contacts__cell--email">{{ $contact->email }}</td>
                         <td class="contacts-table__cell">
                             <a href="tel:{{ $contact->phone }}" class="contacts-table__phone">{{ $contact->phone }}</a>
                         </td>
-                        <td class="contacts-table__cell">
+                        <td class="contacts-table__cell contacts__cell--actions">
                             <div class="contacts-table__actions">
                                 <a href="{{ route('contacts.edit', $contact) }}" class="contacts-table__action-button contacts-table__action-button--edit">
                                     Edit
